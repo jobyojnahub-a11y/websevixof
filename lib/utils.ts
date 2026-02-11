5892ba1cd5269969b2dda1a3950b69dc03a582ab
Bhanu Yadav <dev.bhanuyadav@gmail.com>
2026-02-11 21:22:54 +0530

    Fix duplicate code in utils and models - remove duplicate imports and exports

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,9 +5,3 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
